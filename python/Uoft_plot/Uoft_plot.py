928f3d623b69fbc3850e79c17d432acc2f9bb6d8
Nils Wenzler <wenzlern@ethz.ch>
2014-10-22 10:02:35 +0200

    Improved the Uoft.plot script. It now takes a folder name and uses this as label if not specified.

--- python/Uoft_plot/Uoft_plot.py
+++ python/Uoft_plot/Uoft_plot.py
@@ -19,10 +19,15 @@ from PlotClass import *
 parser = argparse.ArgumentParser()
 # Command line arguments
 parser.add_argument('filenames', nargs='+', type=str,  
-                    help='The path to the Uoft.log file to plot')
-parser.add_argument('-l', '--legend', nargs='+', type=str,  
+                    help='The folder that contains the Uoft.log file')
+parser.add_argument('-l', '--legend', nargs='*', type=str,
                     help='Provide a list of names: [labelA,labelB]')
+parser.add_argument('-t', '--title', nargs='*', type=str,
+                    help='Title for the graph')
 args = parser.parse_args()
+
+#Set legend to folder names if not specified
+legend = args.legend if args.legend else args.filenames
 
 # Define empty data list
 transf_c = []
@@ -32,7 +37,7 @@ SOC      = []
 
 # Load the 4 columns starting with the 2nd line
 for i in range(len(args.filenames)):
-    data = loadtxt(args.filenames[i], skiprows = 1)
+    data = loadtxt(args.filenames[i] + '/Uoft.log', skiprows = 1)
     transf_c.append(np.asarray(data[:,1][:]))
     cell_pot.append(np.asarray(data[:,2][:]))
     curr.append(np.asarray(data[:,3][:]))
@@ -42,11 +47,12 @@ for i in range(len(args.filenames)):
 # Define arrays for the individual values
 
 for i in range(len(args.filenames)):
-    plt.plot(SOC[i], cell_pot[i], label=args.legend[i])
+    plt.plot(SOC[i], cell_pot[i], label=legend[i])
 
 plt.grid(True)
 plt.axis([0,1,0,1.5])
 plt.xlabel('SOC[-]')
 plt.ylabel('Cell Voltage[V]')
 plt.legend()
+plt.title(' '.join(args.title))
 plt.show()
