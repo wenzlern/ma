16374857aebc5b4a5ffcd2e77ddddf35162838aa
Nils Wenzler <wenzlern@ethz.ch>
2014-10-24 11:23:09 +0200

    Clean up before pushing to github

---
+++ scripts/check_load.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#Simple script that ssh's into all available computing servers
+#and reports hostname and uptime (load average)
+
+ssh vanadium "hostname;uptime"
+ssh dahu1    "hostname;uptime"
+ssh dahu2    "hostname;uptime"
+ssh dahu3    "hostname;uptime"
+ssh dahu4    "hostname;uptime"
+ssh dahu5    "hostname;uptime"
+ssh dahu6    "hostname;uptime"
+ssh dahu7    "hostname;uptime"
+ssh dahu8    "hostname;uptime"
+ssh dahu9    "hostname;uptime"
+ssh dahu10   "hostname;uptime"
+ssh dahu11   "hostname;uptime"
+ssh dahu12   "hostname;uptime"
