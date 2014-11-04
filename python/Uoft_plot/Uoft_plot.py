ae7c69ff9ccc5c1e743f8084530f64f184009b48
Nils Wenzler <wenzlern@ethz.ch>
2014-11-04 16:57:20 +0100

    Added some options to UoftPlot.py

--- python/Uoft_plot/Uoft_plot.py
+++ python/Uoft_plot/Uoft_plot.py
@@ -13,7 +13,29 @@ import argparse
 import sys
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.integrate
+from scipy.spatial import distance
 from PlotClass import *
+
+def spacedmarks(x, y, Nmarks, data_ratio=None):
+    if data_ratio is None:
+        data_ratio = plt.gca().get_data_ratio()
+    
+    dydx = gradient(y, x[1])
+    dxdx = gradient(x, x[1])*data_ratio
+    arclength = scipy.integrate.cumtrapz(sqrt(dydx**2 + dxdx**2), x)
+    #We dont have the initial=0 option due to version. Append 0 at beginning
+    arclength = np.hstack(([0],arclength))
+    marks = linspace(0, max(arclength), Nmarks)
+    markx = interp(marks, arclength, x)
+    marky = interp(markx, x, y)
+    return markx, marky
+
+def FindIdealSpacing(x, y):
+    dist = []
+    for i in range(len(x)-1):
+       dist.append(distance.euclidean([x[i],y[i]],[x[i+1],y[i+1]]))
+    return min(dist), max(dist)
 
 ## Parse command line arguments
 parser = argparse.ArgumentParser()
@@ -23,6 +45,12 @@ parser.add_argument('filenames', nargs='+', type=str,
 
 parser.add_argument('-l', '--legend', nargs='*', type=str,
                     help='Provide a list of names: labelA labelB ...')
+
+parser.add_argument('-a', '--axis', nargs='*', type=float,
+                    help='Provide axis range: Xmin Xmax Ymin Ymax')
+
+parser.add_argument('--nolegend', action='store_true',
+                    help='Disable the legend')
 
 parser.add_argument('-c', '--crate', nargs='*', type=int,
                     help='C-Rate of the simulation')
@@ -55,11 +83,20 @@ for i in range(len(args.filenames)):
 
 for i in range(len(args.filenames)):
     plt.plot(SOC[i], cell_pot[i], label=legend[i])
+#for i in range(len(args.filenames)):
+#    x, y = spacedmarks(SOC[i], cell_pot[i], 30,1.5)
+#    plt.plot(x, y, label=legend[i], marker='o')
+#
 
 plt.grid(True)
-#plt.axis([0,1,0,1.5])
+if args.axis:
+    plt.axis(args.axis)
+
 plt.xlabel('SOC[-]')
 plt.ylabel('Cell Voltage[V]')
-plt.legend(loc=2)
+
+if not args.nolegend:
+    plt.legend(loc=2)
+
 plt.title(' '.join(args.title))
 plt.show()
