f3ca9186dcd80bbcdc99790cb4755c18f8f8798c
Nils Wenzler <wenzlern@ethz.ch>
2015-02-11 16:53:22 +0100

    Nearly no interference anymore, however code is much slower now and struggles to finish

---
+++ python/BESTparam/VMPToBest.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+"""This file generates the Open-Circuit-Potential for LFP,
+   according to DOI: 10.1149/1.3567007"""
+
+__author__     = "Nils Wenzler"
+__copyright__  = "Nils Wenzler"
+__maintainer__ = "Nils Wenzler"
+__email__      = "wenzlern@ethz.ch"
+
+# Imports
+from ParameterFile import *
+from math import exp
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+def LFPFitting(soc):
+
+    ocp = 3.4323
+    ocp += -(0.8428*exp(-80.2493*(1-soc)**1.3198))
+    ocp += -(3.2474*1e-6*exp(20.2645*(1-soc)**3.8003))
+    ocp += 3.2482*1e-6*exp(20.2646*(1-soc)**3.7995)
+
+    return ocp
+
+def LFPFittingInv(soc):
+
+    ocp = 3.4323
+    ocp += -(0.8428*exp(-80.2493*(soc)**1.3198))
+    ocp += -(3.2474*1e-6*exp(20.2645*(soc)**3.8003))
+    ocp += 3.2482*1e-6*exp(20.2646*(soc)**3.7995)
+
+    return ocp
+
+def LFPFittingInvLower(soc):
+
+    ocp = 3.4323
+    ocp += -(0.8428*exp(-80.2493*(soc)**1.3198))
+    ocp += -(3.2474*1e-6*exp(20.2645*(soc)**3.8003))
+    ocp += 3.2482*1e-6*exp(20.2646*(soc)**3.7995)
+
+    return ocp - 1.5
+
+def LFPFittingTest(soc):
+
+    ocp = 3.45 - 0.2*soc - 3.45*(soc-0.5)**3
+
+    return ocp
+
+comment = 'LFP SignatureCurve @25 degree celsius, from DOI: 10.1149/1.3567007'
+
+Paramfile = Parametrization('LFP_OCP_RT', comment = comment)
+
+Paramfile.AddFunc(LFPFitting, 0, 1, 0.01)
+# To calculate the numerical differentiation we need one more value at each
+# end of the range.
+var =np.concatenate(([4.2], Paramfile.Values))
+var_c = var[1:len(var)] - var[0:-1]
+#Paramfile.AddValues_c(np.diff(np.concatenate(([2.5], Paramfile.Values, [LFPFitting(1.01)])))[1:len(Paramfile.Values)+1])
+Paramfile.AddValues_c(var_c)
+
+Paramfile.SaveToFile('LFP_OCP_RT.c')
+
+if sys.argv[1] == '-p':
+    print Paramfile.Values[0], Paramfile.Values[1]
+    print Paramfile.Values_c[0], Paramfile.Values_c[1]
+    plt.plot(range(len(Paramfile.Values)), Paramfile.Values)
+    plt.plot(range(len(Paramfile.Values_c)), Paramfile.Values_c)
+    plt.show()
